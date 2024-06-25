abd40ef88be927e9dd19ac0284762332cff3bbfc
David Jerković <jerkdavi@gmail.com>
2024-06-25 20:46:40 +0200

    Commit of LeetCode Problem 1768. Solution

---
+++ RunningSumOf1DArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RunningSumOf1DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1480.
+            //int[] nums = new int[] { 1, 2, 3, 4 };
+            //int[] nums = new int[] { 1, 1, 1, 1, 1 };
+            //int[] nums = new int[] { 3, 1, 2, 10, 1 };
+            Console.Write("What is the length of the array: ");
+            int numsLength = int.Parse(Console.ReadLine());
+            int[] nums = new int[numsLength];
+            for(int i = 0; i < numsLength; i++)
+            {
+                Console.Write($"Enter array[{i + 1}] value: ");
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            nums = Solution.RunningSum(nums);
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(i != 0)
+                {
+                    Console.Write(",");
+                }
+                Console.Write(nums[i]);
+            }
+
+            Console.WriteLine("\nDone processing.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RunningSumOf1DArray/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RunningSumOf1DArray
+{
+    public class Solution
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;
+        }
+    }
+}
