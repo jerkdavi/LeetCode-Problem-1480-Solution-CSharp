23fde1f6bab73e4c1d14f57f838804277c505d3c
David Jerković <jerkdavi@gmail.com>
2024-06-26 17:45:11 +0200

    Updated Program class - basic array initialization

--- RunningSumOf1DArray/Program.cs
+++ RunningSumOf1DArray/Program.cs
@@ -7,13 +7,13 @@ namespace RunningSumOf1DArray
         static void Main(string[] args)
         {
             //1480.
-            //int[] nums = new int[] { 1, 2, 3, 4 };
-            //int[] nums = new int[] { 1, 1, 1, 1, 1 };
-            //int[] nums = new int[] { 3, 1, 2, 10, 1 };
+            //int[] nums = { 1, 2, 3, 4 };
+            //int[] nums = { 1, 1, 1, 1, 1 };
+            //int[] nums = { 3, 1, 2, 10, 1 };
             Console.Write("What is the length of the array: ");
             int numsLength = int.Parse(Console.ReadLine());
             int[] nums = new int[numsLength];
-            for(int i = 0; i < numsLength; i++)
+            for (int i = 0; i < numsLength; i++)
             {
                 Console.Write($"Enter array[{i + 1}] value: ");
                 nums[i] = int.Parse(Console.ReadLine());
@@ -22,7 +22,7 @@ namespace RunningSumOf1DArray
             nums = Solution.RunningSum(nums);
             for(int i = 0; i < nums.Length; i++)
             {
-                if(i != 0)
+                if (i != 0)
                 {
                     Console.Write(",");
                 }
